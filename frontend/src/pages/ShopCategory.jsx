82dd9ea1a37f5c57cab1f17d88078c95d3448439
Madhav <madhavkolasani@gmail.com>
2025-10-09 12:44:05 +0530

    added footer, newsletter

--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import './CSS/ShopCategory.css'
+import ShopContext from '../context/ShopContext'
+
 
 const ShopCategory = () => {
+  const {all_products} = useContext(ShopContext);
+
   return (
-    <div>ShopCategory</div>
+    <div className='shop-category'>
+      
+    </div>
   )
 }
 
